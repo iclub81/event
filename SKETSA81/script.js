675b7df1d8ba3b20ff6dd12c5a236f25baba3bb1
PannDev <dryhezelnut@gmail.com>
2024-11-10 19:21:12 +0700

    INI FOKUS SPONSOR SECTION

--- SKETSA81/script.js
+++ SKETSA81/script.js
@@ -1,11 +1,1 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slider-image");
-
-function moveSlide(direction) {
-    slides[currentSlide].classList.remove("active");
-    currentSlide = (currentSlide + direction + slides.length) % slides.length;
-    slides[currentSlide].classList.add("active");
-}
-
-// Inisialisasi tampilan slide pertama
-slides[currentSlide].classList.add("active");
+// Sponsor Section JS
